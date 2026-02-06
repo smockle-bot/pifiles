32b84c5f64f77ca157dfaca991ac08299fcdaddc
Clay Miller <clay@smockle.com>
2026-02-06 13:43:33 -0500

    fix: Update Node.js version

--- pifiles.sh
+++ pifiles.sh
@@ -34,7 +34,7 @@ sudo apt autoremove
 # Add NodeSource repository
 if [ ! -f /etc/apt/sources.list.d/nodesource.list ]; then
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
-    echo "deb [arch=arm64 signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    echo "deb [arch=arm64 signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_24.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 fi
 
 # Update package lists
@@ -78,7 +78,6 @@ npm i --location=global npm@latest
 
 # Set up Homebridge
 npm install --location=global homebridge homebridge-ring homebridge-mi-airpurifier homebridge-dummy homebridge-levoit-humidifiers homebridge-rachio-irrigation
-# homebridge-ring includes https://github.com/homebridge/ffmpeg-for-homebridge
 # homebridge-rachio-irrigation requires Webhook Relay
 # 1. Install the client: `curl https://my.webhookrelay.com/webhookrelay/downloads/install-cli.sh | bash`
 # 2. Log in: `relay login`
